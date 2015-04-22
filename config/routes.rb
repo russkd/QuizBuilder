62ef2ab9844c400be1765c6a0c9a21d807f588df
russkd <russ.dollinger@ingenuitor.com>
2015-04-21 20:39:23 -0700

    Finish user sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
+# Static_page routes
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+
+# User routes
+  get 'users' => 'users#index'
+  get 'users/new' => 'users#new', as: "new_user"
+  get 'users/:id' => 'users#show', as: "user"
   get 'signup' => 'users#new'
+  post 'users' => 'users#create' 
+  get 'users/:id/edit' => 'users#edit', as: "edit"
+  patch 'users/:id/' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+
+
+  
 end
